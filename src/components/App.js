f76c70fce967ea233b5784746f41e70f39ed0ff8
Mark Brian <markbkiunga2017@gmail.com>
2024-07-30 09:23:52 +0300

    Completes Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 function NavBar() {
   return (
@@ -16,15 +16,23 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+/* write an <About> component here */
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
